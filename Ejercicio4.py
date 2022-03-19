ccf52c4362c10e280ebdc7f7da3ad92dc033103f
KarenBarbosa2004 <101910201+KarenBarbosa2004@users.noreply.github.com>
2022-03-18 20:09:51 -0500

    Add files via upload

---
+++ Ejercicio4.py
@@ -0,0 +1,7 @@
+from calendar import c
+
+
+valor=(input)("digite numero entero")
+resultado=str(valor)
+c=int(valor)+int(valor*2)+int(valor*3)
+print(format(c))
---
+++ ejercicio3.py
@@ -0,0 +1,5 @@
+ganados=int(input("digite el numero de partidos ganados"))
+perdidos=int(input("digite el numero de partidos perdidos"))
+empatados=int(input("digite el numero de partidos empatados"))
+puntos=((ganados*3)+(empatados*1))
+print("el total de puntos es:", puntos)
